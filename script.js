5af3a96cb74b4e0be07a9ff09fbb750668be96bb
khushalchopraa <advkhushal97@gmail.com>
2024-07-22 13:51:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
-function firstWord(s) {
-  // your code here
+
+function firstWord(str) {
+    // Trim leading and trailing whitespace
+    str = str.trim();
+    
+    // Find the index of the first space
+    let spaceIndex = str.indexOf(' ');
+    
+    // If there's no space, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+    
+    // Return the substring up to the first space
+    return str.substring(0, spaceIndex);
 }
+
+// Examples
+console.log(firstWord('see and stop'));  // Output: 'see'
+console.log(firstWord(' Hello World!'));  // Output: 'Hello'
+console.log(firstWord('12345'));  // Output: '12345'
+console.log(firstWord(''));  // O	s.includes==
+
 
 // Do not change the code below
 
